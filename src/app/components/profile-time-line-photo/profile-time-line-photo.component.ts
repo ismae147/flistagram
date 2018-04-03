db6750b2987777d31a18022946b7216f864fc367
isme147 <ismael14_16@hotmail.com>
2018-04-03 01:19:08 -0500

    Add detect scss file for time line

--- src/app/components/profile-time-line-photo/profile-time-line-photo.component.ts
+++ src/app/components/profile-time-line-photo/profile-time-line-photo.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 
 @Component({
-  selector: 'app-profile-time-line-photo',
-  templateUrl: './profile-time-line-photo.component.html',
-  styles: []
+  selector: "app-profile-time-line-photo",
+  templateUrl: "./profile-time-line-photo.component.html",
+  styleUrls: [
+    "./profile-time-line-photo.component.scss"
+  ]
 })
 export class ProfileTimeLinePhotoComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
